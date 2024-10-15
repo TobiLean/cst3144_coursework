9985cadba0a8d26fa8f75ec7f0754cbb11468370
TobiLean <Tobiloba.tsl@gmail.com>
2024-10-15 14:43:01 +0400

    Initial commit

---
+++ lessons.js
@@ -0,0 +1,23 @@
+lessonArray= [
+    {
+        id: "MATH01",
+        subject: "Math",
+        location: "London",
+        price: 500,
+        spaces: 10
+    },
+    {
+        id: "MATH02",
+        subject: "English",
+        location: "London",
+        price: 500,
+        spaces: 10
+    },
+    {
+        id: "MATH03",
+        subject: "Geography",
+        location: "UAE",
+        price: 500,
+        spaces: 10
+    },
+]
