e1434db52708c14af02bf38081579cf01230f201
TobiLean <Tobiloba.tsl@gmail.com>
2024-10-20 20:17:03 +0400

    added mongodb connection

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
-const {MongoClient} = require('mongodb');
+const { getLessonsCollection, getLessonsConnection} = require('./src/db');
+//const {MongoClient} = require('mongodb');
 
 const app = express();
 const PORT= 8090;
 
 app.use(bodyParser.json());
 
-app.use('cw', express.static(path.join(__dirname, 'public')))
+app.use('/cw', express.static(path.join(__dirname, 'public')));
+
+const main = async () => {
+  try {
+    const _lessons = await getLessonsConnection();
+    const allLessons = await _lessons.find().toArray();
+    console.log(allLessons)
+  } catch (err) {
+    console.error("Error occured while trying to get lessons .", err);
+  }
+}
+
+main();
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
---
+++ src/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const {MongoClient} = require('mongodb');
+
+const uri = `mongodb+srv://tobilobatsl:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.dxbm5ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const connectToMongo = async () => {
+  try {
+    const client = new MongoClient(uri);
+    await client.connect();
+    const _db = client.db('cst3144_cw');
+    // _lessons = _db.collection('lessons');
+    console.log("connected to mongo successfully");
+    return _db;
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+const getLessonsConnection = async () => {
+  let db = await connectToMongo();
+  return db.collection('lessons');
+}
+
+module.exports = {getLessonsConnection};
