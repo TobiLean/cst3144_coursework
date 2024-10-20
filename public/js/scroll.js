c3e3ba21bf23c67895ffd0447912af2c2fe85698
TobiLean <Tobiloba.tsl@gmail.com>
2024-10-20 20:27:36 +0400

    removed public and node modules from tracking

--- public/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("navbar_shadow_class")
-    } else {
-        navbar.classList.remove("navbar_shadow_class")
-    }
-})
